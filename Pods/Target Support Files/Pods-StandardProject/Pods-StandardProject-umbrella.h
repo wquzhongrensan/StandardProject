b21aada5c4e3f4df6eff639eb4a21998853e2713
Admin <appscommoem1@gmail.com>
2017-08-03 16:59:02 +0800

    初始化项目,这是一个共通型app

---
+++ Pods/Target Support Files/Pods-StandardProject/Pods-StandardProject-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_StandardProjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_StandardProjectVersionString[];
+
